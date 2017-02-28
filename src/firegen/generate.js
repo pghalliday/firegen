8e4be7012770e5747071ad74f5c829ed42c7eb0a
Peter Halliday <pghalliday@gmail.com>
2017-02-28 23:18:46 +0100

    upgrade alarmist

--- src/firegen/generate.js
+++ src/firegen/generate.js
@@ -7,22 +7,15 @@ const pwriteFile = promisify(writeFile);
 const pmkdirp = promisify(mkdirp);
 
 async function generate({root, config}) {
-  const files = [
-    path.resolve(root, config.output.api),
-    path.resolve(root, config.output.database),
-    path.resolve(root, config.output.storage),
-  ];
-  const dirsAndFiles = files.map((file) => ({
-    dir: path.dirname(file),
-    file: file,
+  await Promise.all([
+    ['api', ''],
+    ['database', ''],
+    ['storage', ''],
+  ].map(async ([prop, content]) => {
+    const file = path.resolve(root, config.output[prop]);
+    await pmkdirp(path.dirname(file));
+    await pwriteFile(file, content);
   }));
-  const promises = dirsAndFiles.map((dirAndFile) => {
-    return (async () => {
-      await pmkdirp(dirAndFile.dir);
-      await pwriteFile(dirAndFile.file, '');
-    })();
-  });
-  await Promise.all(promises);
 }
 
 export default generate;
